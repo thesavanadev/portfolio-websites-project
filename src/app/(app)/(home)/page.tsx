f226ebf1d5962c7a888a194e27e30c74724fadfa
Michael Caesar Owuor <michael.owuor@gmail.com>
2024-06-18 17:05:05 +0300

    refactor: create floating navigation bar

--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -1,9 +1,14 @@
+import { navigationItems } from "@/lib/data";
+
 import HeroSection from "@/components/hero-section";
+import { FloatingNavigationBar } from "@/components/ui/floating-navigation-bar";
 
 const HomePage = () => {
 	return (
 		<article>
 			<section className="w-full max-w-7xl">
+				<FloatingNavigationBar navigationItems={navigationItems} />
+
 				<HeroSection />
 			</section>
 		</article>
